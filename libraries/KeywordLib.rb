8acbbb66c2574d7b487ba807ac7287d31c0cbfd7
Cannon Mallory <56271516+malloc3@users.noreply.github.com>
2020-03-17 08:09:27 -0700

    Aquadoc initial commit

---
+++ libraries/KeywordLib.rb
@@ -0,0 +1,11 @@
+#Cannon Mallory
+#malloc3@uw.edu
+#
+#This is where all the standard keywords/values will live.
+
+module KeywordLib
+    MAX_INPUTS = 96
+    C_TYPE = "96 Well Sample Plate"
+    CON_KEY = "Stock Conc (ng/ul)"
+    QC2_KEY = "C-DNA QC"
+end
