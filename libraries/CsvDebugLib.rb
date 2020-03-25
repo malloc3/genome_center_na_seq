e668c5ba9797f7677905bcb7691c511bd26da8b0
Cannon Mallory <56271516+malloc3@users.noreply.github.com>
2020-03-25 10:42:46 -0700

    Aquadoc created file

---
+++ libraries/CsvDebugLib.rb
@@ -0,0 +1,24 @@
+module CsvDebugLib
+    CSV_DEBUG = "Plate ID,Well Location
+    100,A1
+    100,B1
+    100,C1
+    100,D1
+    100,E1
+    100,F1
+    100,G1
+    100,A2
+    100,B2
+    100,C2
+    100,D2
+    100,E2
+    100,F2
+    100,G2
+    100,A3
+    100,B3
+    100,C3
+    100,D3
+    100,E3
+    100,F3
+    100,G3"
+end
